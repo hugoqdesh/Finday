9265a51c6580ef32259eddd2e254c93ddccd8482
hugoqdesh <gitjohan5@gmail.com>
2025-10-31 16:13:39 +0200

    fixed sidebar header

--- components/sidebar/site-header.tsx
+++ components/sidebar/site-header.tsx
@@ -5,7 +5,7 @@ import { Bell } from "lucide-react";
 
 export function SiteHeader() {
 	return (
-		<header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b py-3">
+		<header className="sticky top-0 z-40 flex h-(--header-height) shrink-0 items-center gap-2 border-b bg-background py-3">
 			<div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
 				<SidebarTrigger className="-ml-1" />
 				<div className="ml-auto flex items-center gap-4">
